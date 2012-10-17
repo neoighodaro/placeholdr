6f146a58d066eb6b0886fb4ea1ec25b399b23156
Neo Ighodaro <thecreativityteam@yahoo.com>
2012-10-17 07:04:22 +0100

    Few code improvements and Typoraphy fixes

--- routes.php
+++ routes.php
@@ -19,12 +19,12 @@ Route::get('(:bundle)', function()
  */
 Route::get('(:bundle)/(:any)/(:any?)', array('as' => 'placeholdr', function($dimension, $text = null)
 {
-	// Starighten up dimensions
+	// Straighten up dimensions, we want it to be case-insensitive
 	$dimension = strtolower($dimension);
 
-	// A valid dimension needs to have the "x" string, but in cases where
-	// a perfect square placeholdr is needed, the height does not need to be specified
-	// thus we will add his ourselves.
+	// A valid dimension needs to have the "x" string, but in cases where a perfect square
+	//  placeholdr is needed, the height does not need to be specified thus we will add
+	//  the "x" string and height automatically.
 	if ( ! str_contains('x', $dimension))
 	{
 		$dimension = $dimension.'x'.$dimension;
@@ -33,13 +33,16 @@ Route::get('(:bundle)/(:any)/(:any?)', array('as' => 'placeholdr', function($dim
 	// Get height and width from dimensions
 	list($width, $height) = explode('x', $dimension, 2);
 
-	// Custom headers
-	$custom_headers = array(
+	// Get the placeholdr image content
+	$content = Placeholdr::get($width, $height, $text);
+
+	// Status code
+	$status = 200;
+
+	// Headers that will be passed with Response
+	$headers = array(
 		'Content-Type' => 'image/png',
 	);
 
-	// Content
-	$content = Placeholdr::get($width, $height, $text);
-
-	return Response::make($content, 200, $custom_headers);
+	return Response::make($content, $status, $headers);
 }));
--- start.php
+++ start.php
@@ -12,35 +12,73 @@
  * @version 1.0
  */
 
-/**
- * Placeholdr bundle root path.
- *
- * Using this value instead of Bundle::path('placeholdr') gives the developer
- * more options, especially when trying to rename the bundle.
+/*
+|------------------------------------------------------------------------------------------------------------
+| Placeholdr bundle root path.
+|------------------------------------------------------------------------------------------------------------
+|
+| Using this value instead of Bundle::path('placeholdr') gives the developer
+| more options, especially when trying to rename the bundle.
+|
  */
+
 define('PLACEHOLDR', __DIR__.DIRECTORY_SEPARATOR);
 
-/**
- * Add libraries folder to autoloader loading directories.
- *
- * This will tell Laravel where to look when attempting to autoload Placeholdr
- * classes.
+
+/*
+|------------------------------------------------------------------------------------------------------------
+| Add libraries folder to autoloader loading directories.
+|------------------------------------------------------------------------------------------------------------
+|
+| This will tell Laravel where to look when attempting to autoload Placeholdr
+| classes.
+|
  */
+
 Autoloader::directories(array(
 	PLACEHOLDR.'libraries',
 ));
 
 /**
+ * Placeholdr helper to create placeholdrs on the fly.
+ *
+ * This helper creates a placeholdr image by creating a HTML img tag
+ * in the views of your Laravel application.
+ *
+ * <code>
+ * 	{{ placeholdr('300x300', 'Optional Text') }}
+ * </code>
+ *
+ * @param string $dimension
+ * @param string $text
+ * @param array $attributes
+ * @return string
+ */
+function placeholdr($dimension, $text = '', $attributes = array())
+{
+	// URL encode optional text
+	$text = rawurlencode($text);
+
+	return '<img src="'.URL::to_route('placeholdr', array($dimension, $text)).'"'.HTML::attributes($attributes).' />';
+}
+
+/**
  * Register a HTML macro for use in views.
  *
  * The Placeholdr HTML macro makes using Placeholdr within the application
- * views a breeze. Simple and elegant, the Laravel way ;)
+ * views a breeze. Simple and elegant, the Laravel way ;). This is just a clean Laravelish
+ * wrapper around the placeholdr helper function.
  *
  * <code>
- * 	{{ HTML::placeholdr('300x300') }}
+ * 	{{ HTML::placeholdr('300x300', 'Optional Text') }}
  * </code>
+ *
+ * @param string $dimension
+ * @param string $text
+ * @param array $attributes
+ * @return string
  */
-HTML::macro('placeholdr', function($dimension, $text = '', $attributes = array())
+HTML::macro('placeholdr', function($dimension, $text = '', $attribute = array())
 {
-	return '<img src="'.URL::to_route('placeholdr', array($dimension, $text)).'"'.HTML::attributes($attributes).' />';
+	return placeholdr($dimension, $text, $attribute);
 });
